0f485bb5d97b381f07924b52563ce566e8302d59
rnz-d <thejrdelossantos@gmail.com>
2022-11-26 13:31:45 -0500

    Added models to main

--- webtoonDB/main/models.py
+++ webtoonDB/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Webtoon(models.Model):
+    ASURA = "AR"
+    MREADER = "MR"
+    SITE_CHOICES = [
+        (ASURA, "AsuraScans"),
+        (MREADER, "MReader"),
+    ]
+    title = models.CharField(max_length=256)
+    site = models.CharField(
+        max_length=2,
+        choices=SITE_CHOICES,
+        default=MREADER,
+    )
+    url = models.URLField(
+        max_length=1024,
+        default="https://www.mreader.co/jumbo/manga/",
+    )
+    available_ch = models.FloatField(default=0)
+    finished_ch = models.FloatField(default=0)
+
+    def __str__(self) -> str:
+        return self.title
